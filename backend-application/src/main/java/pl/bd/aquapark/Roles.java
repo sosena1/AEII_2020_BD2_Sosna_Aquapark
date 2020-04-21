2cb1607437cd279b3de897c8f07cc1350e1530ee
sita <gtnetr@gmail.com>
2020-04-21 23:05:56 +0200

    GET /api/client/{id} implemented (not tested yet)

---
+++ backend-application/src/main/java/pl/bd/aquapark/Roles.java
@@ -0,0 +1,23 @@
+package pl.bd.aquapark;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Roles implements GrantedAuthority {
+    USER("USER"),
+    SELLER("SELLER");
+
+
+    private final String role;
+
+    private Roles(String role) {
+        this.role = role;
+    }
+
+    public String getAuthority() {
+        return this.role;
+    }
+
+    public String toString() {
+        return this.role;
+    }
+}
