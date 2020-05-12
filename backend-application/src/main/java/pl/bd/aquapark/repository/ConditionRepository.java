5f519ef67b4802ff979cb1c31ce75d6ad4caa884
sita <gtnetr@gmail.com>
2020-05-12 23:03:10 +0200

    added pricelist endpoint

---
+++ backend-application/src/main/java/pl/bd/aquapark/repository/ConditionRepository.java
@@ -0,0 +1,10 @@
+package pl.bd.aquapark.repository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+import pl.bd.aquapark.dao.Conditions;
+import pl.bd.aquapark.dao.Employee;
+
+@Repository
+public interface ConditionRepository extends PagingAndSortingRepository<Conditions, Long> {
+}
