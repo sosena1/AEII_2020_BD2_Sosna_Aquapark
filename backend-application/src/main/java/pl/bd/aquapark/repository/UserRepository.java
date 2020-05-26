e9879b4f409369061c60c3f23c966025e39403af
sita <gtnetr@gmail.com>
2020-05-27 00:58:13 +0200

    fix for isClient = false

--- backend-application/src/main/java/pl/bd/aquapark/repository/UserRepository.java
+++ backend-application/src/main/java/pl/bd/aquapark/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package pl.bd.aquapark.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import pl.bd.aquapark.dao.User;
@@ -8,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findUserByUserNameAndPassword(String username, String password);
 
     List<User> findUserByUserName(String username);
