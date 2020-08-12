63d1e8cba00bc30cde10b7c2efc672f089f8bac3
jakubklimek97 <jakukli853@student.polsl.pl>
2020-08-12 23:50:39 +0200

    ~ Menu uses AuthService to check if user is logged

--- frontend-application/aquapark/src/app/menu/menu.component.ts
+++ frontend-application/aquapark/src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-menu',
@@ -7,12 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() {
-    this.isSignedIn = true;
+  constructor(authService: AuthService) {
+    this.authService = authService;
   }
 
   ngOnInit(): void {
   }
 
-  isSignedIn : boolean
+  isSignedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
+
+  authService: AuthService;
 }
