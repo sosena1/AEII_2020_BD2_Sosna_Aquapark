c7bbba6a1d1c9c91d88daab4234b369acd82e9d8
jakubklimek97 <jakukli853@student.polsl.pl>
2020-08-12 23:33:23 +0200

    ~ Added router link to landing page

--- frontend-application/aquapark/src/app/app-routing.module.ts
+++ frontend-application/aquapark/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from "./components/login/login.component";
+import {LandingPageComponent} from "./components/landing-page/landing-page.component";
 
 const routes: Routes = [
+  {path: '', component: LandingPageComponent},
   {path: 'login', component: LoginComponent}
 ];
 
