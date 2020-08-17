fe6f6e196e0ccddee47ac76c0fdc90dd1582483f
Michał Jankowski <32636855+Michal-Jankowski@users.noreply.github.com>
2020-08-18 00:04:28 +0200

    Dodanie brakujących klientów i pracowników

--- model/Dane_do_bazy_BD_AEiI_Informatyka_GKiO1.sql
+++ model/Dane_do_bazy_BD_AEiI_Informatyka_GKiO1.sql
@@ -13,6 +13,8 @@ insert into CLIENTIDENTIFICATOR (identificatorId, isInUse) values (2, 0);
 insert into CLIENTIDENTIFICATOR (identificatorId, isInUse) values (3, 0);
 insert into CLIENTIDENTIFICATOR (identificatorId, isInUse) values (4, 0);
 insert into CLIENTIDENTIFICATOR (identificatorId, isInUse) values (5, 0);
+insert into CLIENTIDENTIFICATOR (identificatorId, isInUse) values (6, 0); 
+insert into CLIENTIDENTIFICATOR (identificatorId, isInUse) values (7, 0);
 
 insert into AQUAPARKATTRACTIONGATEEVENT (eventId, isEntering, date, time, gateId, identificatorId) values (1, 1, '2020-08-06', '11:00', 2, 1);
 insert into AQUAPARKATTRACTIONGATEEVENT (eventId, isEntering, date, time, gateId, identificatorId) values (2, 0, '2020-08-06', '13:00', 2, 1);
@@ -23,7 +25,9 @@ insert into AQUAPARKATTRACTIONGATEEVENT (eventId, isEntering, date, time, gateId
 insert into AQUAPARKATTRACTIONGATEEVENT (eventId, isEntering, date, time, gateId, identificatorId) values (7, 1, '2020-08-09', '17:45', 3, 4);
 insert into AQUAPARKATTRACTIONGATEEVENT (eventId, isEntering, date, time, gateId, identificatorId) values (8, 0, '2020-08-09', '18:53', 3, 4);
 insert into AQUAPARKATTRACTIONGATEEVENT (eventId, isEntering, date, time, gateId, identificatorId) values (9, 1, '2020-08-09', '09:52', 5, 5);
-insert into AQUAPARKATTRACTIONGATEEVENT (eventId, isEntering, date, time, gateId, identificatorId) values (10, 0, '2020-08-09', '12:38', 5, 5);
+insert into AQUAPARKATTRACTIONGATEEVENT (eventId, isEntering, date, time, gateId, identificatorId) values (10, 0, '2020-08-09', '13:38', 2, 6); 
+insert into AQUAPARKATTRACTIONGATEEVENT (eventId, isEntering, date, time, gateId, identificatorId) values (11, 0, '2020-08-09', '13:40', 2, 7);
+insert into AQUAPARKATTRACTIONGATEEVENT (eventId, isEntering, date, time, gateId, identificatorId) values (12, 0, '2020-08-09', '15:39', 3, 6);
 
 insert into GENDER (genderId, genderName) values (1, 'Female');
 insert into GENDER (genderId, genderName) values (2, 'Male');
@@ -43,18 +47,24 @@ insert into USER (userId, firstName, lastName, address, contactNumber, otherInfo
 insert into EMPLOYEE (employeeId, userId) values (1, 5);
 insert into EMPLOYEE (employeeId, userId) values (2, 7);
 insert into EMPLOYEE (employeeId, userId) values (3, 2);
+insert into EMPLOYEE (employeeId, userId) values (4, 8); 
+insert into EMPLOYEE (employeeId, userId) values (5, 9);
 
 insert into CLIENT (clientId, ownsAccount, userId) values (1, 0, 1);
 insert into CLIENT (clientId, ownsAccount, userId) values (2, 1, 3);
 insert into CLIENT (clientId, ownsAccount, userId) values (3, 1, 5);
 insert into CLIENT (clientId, ownsAccount, userId) values (4, 0, 7);
 insert into CLIENT (clientId, ownsAccount, userId) values (5, 1, 10);
+insert into CLIENT (clientId, ownsAccount, userId) values (6, 0, 4); 
+insert into CLIENT (clientId, ownsAccount, userId) values (7, 1, 6);
 
 insert into VISIT (visitId, date, startTime, endTime, value, identificatorId, clientId) values (1, '2020-05-06', '10:30', '13:30', 50.50, 1, 1);
 insert into VISIT (visitId, date, startTime, endTime, value, identificatorId, clientId) values (2, '2020-07-09', '11:30', '14:00', 17.30, 2, 2);
 insert into VISIT (visitId, date, startTime, endTime, value, identificatorId, clientId) values (3, '2020-07-11', '08:30', '12:00', 10.20, 3, 2);
-insert into VISIT (visitId, date, startTime, endTime, value, identificatorId, clientId) values (5, '2020-07-21', '09:30', '13:00', 20.60, 5, 4);
-insert into VISIT (visitId, date, startTime, endTime, value, identificatorId, clientId) values (4, '2020-07-18', '17:30', '19:00', 9.80, 4, 3);
+insert into VISIT (visitId, date, startTime, endTime, value, identificatorId, clientId) values (4, '2020-07-21', '09:30', '13:00', 20.60, 5, 4); 
+insert into VISIT (visitId, date, startTime, endTime, value, identificatorId, clientId) values (5, '2020-07-18', '17:30', '19:00', 9.80, 4, 3);
+insert into VISIT (visitId, date, startTime, endTime, value, identificatorId, clientId) values (6, '2020-07-23', '10:30', '13:00', 22.60, 7, 6);
+insert into VISIT (visitId, date, startTime, endTime, value, identificatorId, clientId) values (7, '2020-07-23', '10:30', '13:00', 12.80, 6, 7);
 
 insert into CONDITIONS (conditionId, weekendOnly, childOnly, seniorOnly) values (1, 1, 0, 0);
 insert into CONDITIONS (conditionId, weekendOnly, childOnly, seniorOnly) values (2, 0, 1, 0);
@@ -65,20 +75,20 @@ insert into PRICELIST (priceListId, validityStartDate, employeeId) values (1, '2
 insert into PRICELIST (priceListId, validityStartDate, employeeId) values (2, '2019-07-02', 1);
 insert into PRICELIST (priceListId, validityStartDate, employeeId) values (3, '2019-10-16', 2);
 
-insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (1, 'Pool', 174.06, 'Pool price', 1, 4, 1);
-insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (2, 'Slide', 72.18, 'Slide price', 2, 4, 2);
-insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (3, 'Pool', 20.00, 'Pool price', 3, 1, 1);
-insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (4, 'Pool', 5.00, 'Pool price', 3, 2, 1);
-insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (5, 'Pool', 7.30, 'Pool price', 3, 3, 1);
-insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (6, 'Pool', 10.20, 'Pool price', 3, 4, 1);
-insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (7, 'Slide', 30.00, 'Slide price', 3, 1, 2);
-insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (8, 'Slide', 15.00, 'Slide price', 3, 2, 2);
-insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (9, 'Slide', 15.00, 'Slide price', 3, 3, 2);
-insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (10, 'Slide', 15.00, 'Slide price', 3, 4, 2);
-insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (11, 'Big pool', 20.00, 'Big pool price', 3, 1, 3);
-insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (12, 'Big pool', 10.20, 'Big pool price', 3, 2, 3);
-insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (13, 'Big pool', 5.50, 'Big pool price', 3, 3, 3);
-insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (14, 'Big pool', 16.20, 'Big pool price', 3, 4, 3);
+insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (1, 'Pool', 174.06, 'Pool price/All', 1, 4, 1);
+insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (2, 'Slide', 72.18, 'Slide price/All', 2, 4, 2);
+insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (3, 'Pool', 20.00, 'Pool price/Weekend', 3, 1, 1);
+insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (4, 'Pool', 5.00, 'Pool price/Child', 3, 2, 1);
+insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (5, 'Pool', 7.30, 'Pool price/Senior', 3, 3, 1);
+insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (6, 'Pool', 10.20, 'Pool price/All', 3, 4, 1);
+insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (7, 'Slide', 30.00, 'Slide price/Weekend', 3, 1, 2);
+insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (8, 'Slide', 15.00, 'Slide price/Child', 3, 2, 2);
+insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (9, 'Slide', 15.00, 'Slide price/Senior', 3, 3, 2);
+insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (10, 'Slide', 15.00, 'Slide price/All', 3, 4, 2);
+insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (11, 'Big pool', 20.00, 'Big pool price/Weekend', 3, 1, 3);
+insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (12, 'Big pool', 10.20, 'Big pool price/Child', 3, 2, 3);
+insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (13, 'Big pool', 5.50, 'Big pool price/Senior', 3, 3, 3);
+insert into PRICELISTITEM (priceListItemId, name, value, description, priceListId, conditionalId, attractionId) values (14, 'Big pool', 16.20, 'Big pool price/All', 3, 4, 3);
 
 insert into ROLE (roleId, roleName) values (1, 'Cashier');
 insert into ROLE (roleId, roleName) values (2, 'Gate');
@@ -98,12 +108,16 @@ insert into ROLE_HAS_EMPLOYEE (roleId, employeeId) values (6, 2);
 insert into ROLE_HAS_EMPLOYEE (roleId, employeeId) values (7, 2);
 insert into ROLE_HAS_EMPLOYEE (roleId, employeeId) values (8, 2);
 insert into ROLE_HAS_EMPLOYEE (roleId, employeeId) values (3, 3);
+insert into ROLE_HAS_EMPLOYEE (roleId, employeeId) values (5, 4);
+insert into ROLE_HAS_EMPLOYEE (roleId, employeeId) values (5, 5);
 
 insert into AQUAPARKATTRACTIONMAINTENANCE (attractionMaintenanceId, description, date, employeeId, attractionId) values (1, 'Cleaning', '2019-08-31', 2, 3);
 insert into AQUAPARKATTRACTIONMAINTENANCE (attractionMaintenanceId, description, date, employeeId, attractionId) values (2, 'Water change', '2020-02-09', 2, 3);
 insert into AQUAPARKATTRACTIONMAINTENANCE (attractionMaintenanceId, description, date, employeeId, attractionId) values (3, 'Repair', '2019-07-21', 1, 3);
 insert into AQUAPARKATTRACTIONMAINTENANCE (attractionMaintenanceId, description, date, employeeId, attractionId) values (4, 'Modernization', '2020-02-22', 2, 2);
 insert into AQUAPARKATTRACTIONMAINTENANCE (attractionMaintenanceId, description, date, employeeId, attractionId) values (5, 'Water quality test', '2019-11-14', 2, 2);
+insert into AQUAPARKATTRACTIONMAINTENANCE (attractionMaintenanceId, description, date, employeeId, attractionId) values (6, 'Repair', '2020-01-22', 4, 3);
+insert into AQUAPARKATTRACTIONMAINTENANCE (attractionMaintenanceId, description, date, employeeId, attractionId) values (7, 'Cleaning', '2012-10-14', 5, 3);
 
 insert into AQUAPARKATTRACTIONUSAGE (usageId, attractionId, priceListItemId, visitId, enteringEventId, leavingEventId) values (1, 2, 1, 1, 1, 2);
 insert into AQUAPARKATTRACTIONUSAGE (usageId, attractionId, priceListItemId, visitId, enteringEventId, leavingEventId) values (2, 2, 1, 2, 3, 4);
